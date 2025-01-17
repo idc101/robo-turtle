bf53e76336ec60cf206e6a51f7671a80768e7340
Iain Cardnell <idc101@users.noreply.github.com>
2025-01-17 19:22:03 +0000

    Basic driving and obstacle avoidance

--- main.py
+++ main.py
@@ -6,21 +6,27 @@ car.start()
 
 car.rotate_camera_forward()
 
-while not car.check_off_ground():
+
+
+while True:
+  if car.check_off_ground():
+    break;
   x = car.measure_dist()
   print(x)
-  if x and x > 20:
-    car.forward(1)
-  else:
+  car.forward()
+  if x and x < 30:
+    car.stop()
     car.rotate_camera_left()
     l = car.measure_dist()
     car.rotate_camera_right()
     r = car.measure_dist()
+    car.rotate_camera_forward()
     if r > l :
       car.right(90)
     else:
       car.left(90)
-    car.rotate_camera_forward()
+  else:
+    car.forward()
 
 
 car.close()
